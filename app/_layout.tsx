9dcc01a83b6fcd654b4f3378bd79ad29219ed98f
dramirez <dramirezrrzz@gmail.com>
2025-05-07 18:22:08 -0600

    NativeWind funcionando

---
+++ app/_layout.tsx
@@ -0,0 +1,9 @@
+import { Slot } from "expo-router";
+import React from "react";
+import "../global.css";
+
+const _Rootlayout = () => {
+  return <Slot />;
+};
+
+export default _Rootlayout;
--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,14 @@
-import React from "react";
+import { useEffect } from "react";
 import { Text, View } from "react-native";
 
-const App = () => {
+export default function Page() {
+  useEffect(() => {
+    console.log("✅ NativeWind debería estar aplicando clases ahora");
+  }, []);
+
   return (
-    <View>
-      <Text>App</Text>
+    <View className="flex-1 items-center justify-center bg-green-500">
+      <Text className="text-white text-xl">Prueba de estilos</Text>
     </View>
   );
-};
-
-export default App;
+}
