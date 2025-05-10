151554d7e468ab767b9a44e318ec3e659b40cb5b
dramirez <dramirezrrzz@gmail.com>
2025-05-09 19:29:05 -0600

    Custombutton con estilos

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,4 +1,5 @@
 import CustomButton from "@/components/shared/CustomButton";
+import { Link, router } from "expo-router";
 import React from "react";
 import { SafeAreaView, View } from "react-native";
 
@@ -6,7 +7,27 @@ const HomeScreen = () => {
   return (
     <SafeAreaView className="flex-1 bg-white">
       <View className="px-10 mt-5">
-        <CustomButton children={"Productos"} color={"primary"} />
+        <Link href="/products" asChild>
+          <CustomButton className="mb-10" color="primary">
+            Productos
+          </CustomButton>
+        </Link>
+
+        <CustomButton
+          className="mb-10"
+          color="primary"
+          onPress={() => router.push("/products")}
+        >
+          Productos
+        </CustomButton>
+
+        <CustomButton
+          className="mb-10"
+          variant="text-only"
+          onPress={() => router.push("/products")}
+        >
+          Productos
+        </CustomButton>
 
         {/* <Link className="mb-5" href="/products">
           {" "}
--- components/shared/CustomButton.tsx
+++ components/shared/CustomButton.tsx
@@ -1,27 +1,71 @@
 import React from "react";
-import { Pressable, PressableProps, Text } from "react-native";
+import { Pressable, PressableProps, Text, View } from "react-native";
 
 // expandir el componente para que reciba props
 // y se le pueda pasar el texto y el color del fondo y el color de texto
 
 interface CustomButtonProps extends PressableProps {
   children: string;
-  color: "primary" | "secondary" | "tertiary";
+  color?: "primary" | "secondary" | "tertiary";
+  variant?: "contained" | "text-only";
+  className?: string;
 }
 
-const CustomButton = ({ children, color }: CustomButtonProps) => {
-  const btnColor = {
-    primary: "bg-primary",
-    secondary: "bg-secondary",
-    tertiary: "bg-tertiary",
-  }[color];
+const CustomButton = React.forwardRef(
+  (
+    {
+      children,
+      color = "primary",
+      onPress,
+      onLongPress,
+      variant = "contained",
+      className,
+    }: CustomButtonProps,
+    ref: React.Ref<View>
+  ) => {
+    // el color del fondo
+    const btnColor = {
+      primary: "bg-primary",
+      secondary: "bg-secondary",
+      tertiary: "bg-tertiary",
+    }[color];
 
-  return (
-    // la clase active:opacity-90 es para que el boton se vea mas oscuro cuando se le da click
-    <Pressable className={`p-3 rounded-md ${btnColor} active:opacity-90`}>
-      <Text className="text-white text-center">{children}</Text>
-    </Pressable>
-  );
-};
+    // el color del texto
+    const textColor = {
+      primary: "text-primary",
+      secondary: "text-secondary",
+      tertiary: "text-tertiary",
+    }[color];
+
+    // si el color es primario, el texto es blanco, si no es blanco
+    if (variant === "text-only") {
+      return (
+        <Pressable
+          className={`p-3 ${className} `}
+          // el color del fondo
+          onPress={onPress}
+          onLongPress={onLongPress}
+          ref={ref}
+        >
+          <Text className={`text-center ${textColor} font-monse-black`}>
+            {children}
+          </Text>
+        </Pressable>
+      );
+    }
+
+    return (
+      // la clase active:opacity-90 es para que el boton se vea mas oscuro cuando se le da click
+      <Pressable
+        className={`p-3 rounded-md ${btnColor} active:opacity-90 ${className} font-monse-black`}
+        onPress={onPress}
+        onLongPress={onLongPress}
+        ref={ref}
+      >
+        <Text className="text-white text-center">{children}</Text>
+      </Pressable>
+    );
+  }
+);
 
 export default CustomButton;
