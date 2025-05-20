7bf3ab218b060ec914cdc271c9fe01ba58504610
dramirez <dramirezrrzz@gmail.com>
2025-05-19 21:37:39 -0600

    drawer funcionando y con iconos

--- app/drawer/_layout.tsx
+++ app/drawer/_layout.tsx
@@ -1,25 +1,36 @@
+import { Ionicons } from "@expo/vector-icons";
 import { Drawer } from "expo-router/drawer";
 import React from "react";
 
 const Drawerlayout = () => {
   return (
-    <Drawer>
-      <Drawer>
-        <Drawer.Screen
-          name="user/index" // This is the name of the page and must match the url from root
-          options={{
-            drawerLabel: "User",
-            title: "Usuario",
-          }}
-        />
-        <Drawer.Screen
-          name="schedule/index" // This is the name of the page and must match the url from root
-          options={{
-            drawerLabel: "Horario",
-            title: "Horario",
-          }}
-        />
-      </Drawer>
+    <Drawer
+      screenOptions={{
+        overlayColor: "rgba(0,0,0,0.4)",
+        drawerActiveTintColor: "indigo",
+        headerShadowVisible: false,
+      }}
+    >
+      <Drawer.Screen
+        name="user/index" // This is the name of the page and must match the url from root
+        options={{
+          drawerLabel: "User",
+          title: "Usuario",
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="person-circle-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="schedule/index" // This is the name of the page and must match the url from root
+        options={{
+          drawerLabel: "Horario",
+          title: "Horario",
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="calendar-outline" size={size} color={color} />
+          ),
+        }}
+      />
     </Drawer>
   );
 };
