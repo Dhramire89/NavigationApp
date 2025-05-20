3849216f07971a734001e1f86c54b890866a8272
dramirez <dramirezrrzz@gmail.com>
2025-05-19 21:20:34 -0600

    drawer funcionando

--- app/drawer/_layout.tsx
+++ app/drawer/_layout.tsx
@@ -1,11 +1,26 @@
+import { Drawer } from "expo-router/drawer";
 import React from "react";
-import { Text, View } from "react-native";
 
 const Drawerlayout = () => {
   return (
-    <View>
-      <Text>Drawerlayout</Text>
-    </View>
+    <Drawer>
+      <Drawer>
+        <Drawer.Screen
+          name="user/index" // This is the name of the page and must match the url from root
+          options={{
+            drawerLabel: "User",
+            title: "Usuario",
+          }}
+        />
+        <Drawer.Screen
+          name="schedule/index" // This is the name of the page and must match the url from root
+          options={{
+            drawerLabel: "Horario",
+            title: "Horario",
+          }}
+        />
+      </Drawer>
+    </Drawer>
   );
 };
 
