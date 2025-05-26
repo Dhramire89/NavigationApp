093a25698d1b9b39c58858d136261fac3714f750
dramirez <dramirezrrzz@gmail.com>
2025-05-25 19:04:29 -0600

    Titulo basado en los argumentos

--- app/(drawer)/(tabs)/(stack)/products/[id].tsx
+++ app/(drawer)/(tabs)/(stack)/products/[id].tsx
@@ -1,12 +1,31 @@
 import { products } from "@/store/products.store";
+import { useNavigation } from "@react-navigation/native";
 import { useLocalSearchParams } from "expo-router";
-import React from "react";
+import React, { useEffect } from "react";
 import { Text, View } from "react-native";
 
 const ProductScreen = () => {
-  const { id } = useLocalSearchParams();
+  const navigation = useNavigation(); // se usa el useNavigation para navegar entre pantallas
   // se usa el useLocalSearchParams para obtener los params de la url, se le pasa el id del producto
+  const { id } = useLocalSearchParams();
+  // se busca el producto por el id
   const product = products.find((product) => product.id === id); // se busca el producto por el id
+
+  // se usa el useEffect para establecer el título de la pantalla
+  useEffect(() => {}, [product]);
+  navigation.setOptions({
+    title: product ? product.title : "Product Details", // se establece el título de la pantalla
+  });
+
+  if (!product) {
+    // si no se encuentra el producto, se muestra un mensaje de error
+    return (
+      <View>
+        <Text>Product not found</Text>
+      </View>
+    );
+  }
+
   if (!product) {
     // si no se encuentra el producto, se muestra un mensaje de error
     return (
